e30dc01f0d51637339bf37fde30841e860daab38
Stephen DeLorme <stephen@d.elor.me>
2017-03-09 13:23:03 -0500

    Setup a basic testing page for HLS videos

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # hlsdemo
 A web page to demonstrate HLS video playback.
+
+##Changing the M3u8 File
+
+In scripts.js, change the `var hls_source` to reflect the URL of your M3U8 file.
+
+
+##BrowserSync
+
+This is optional, but sometimes it's a good idea to use aBrowserSync while working to prevent Javascript errors caused by cross domain references.
+
+Install using `npm install -g browser-sync`
+
+Then run `browser-sync start --server --files *`
---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//Wrapping everything in this so that it executes when the page is finished loading
+$( document ).ready(function() {
+    
+    //This is the HLS source (M3U* file) that you wish to load
+	var hls_source = 'http://www.streambox.fr/playlists/test_001/stream.m3u8';
+
+	//This loads the appropriate video content into th <video> tag
+    if(Hls.isSupported()) {
+		video = document.getElementById('video');
+			var hls = new Hls();
+			hls.loadSource(hls_source);
+			hls.attachMedia(video);
+			hls.on(Hls.Events.MANIFEST_PARSED,function() {
+			  video.play();
+		});
+	}
+});
